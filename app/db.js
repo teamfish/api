fc9a5c60e0e9b65bbd9c149d35159376ce7a7ce4
Andrea Cresta <a.cresta88@gmail.com>
2014-03-29 12:30:30 +0100

    Update db.js
    Created singleton for access the db

---
+++ app/db.js
@@ -0,0 +1,31 @@
+var MongoClient = require('mongodb').MongoClient;
+
+var db_singleton = null;
+
+var getConnection= function getConnection(callback)
+{
+    if (db_singleton)
+    {
+        callback(null,db_singleton);
+    }
+    else
+    {
+           //placeholder: modify this-should come from a configuration source
+        var connURL = "mongodb://localhost:27017/fishTest"; 
+        MongoClient.connect(connURL,function(err,db){
+
+            if(err)
+                log("Error creating new connection "+err);
+            else
+            {
+                db_singleton=db;    
+                log("created new connection");
+
+            }
+            callback(err,db_singleton);
+            return;
+        });
+    }
+}
+
+module.exports = getConnection;
